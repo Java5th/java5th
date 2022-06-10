a1790c6cb65e8e08f6ab063d7b35cbaf3de224da
bKfe-j <102991624+bKfe-j@users.noreply.github.com>
2022-06-10 17:36:38 +0900

    Update Reserveticket.java

--- Project/src/miniproject1/Reserveticket.java
+++ Project/src/miniproject1/Reserveticket.java
@@ -6,6 +6,9 @@ public class Reserveticket implements ReserveMovie{
 	String userId;
 	String userPw;
 	String[][] seat = new String[5][5];
+	public static void main(String[] args){
+		CheckSeat();
+	}
 
 	@Override
 	public void LogIn(String id,int password) {
@@ -14,51 +17,8 @@ public class Reserveticket implements ReserveMovie{
 	}
 
 	@Override
-	public void ReserveTicket() throws InterruptedException, IOException {
-        while(true) {
-
-                while(true) {
-                    try {
-                        System.out.println("행을 입력하세요(max:5)");
-                        row = scan.nextInt();
-                    } catch (InputMismatchException | ArrayIndexOutOfBoundsException e) {
-                        e.printStackTrace();
-                        scan = new Scanner(System.in);
-                        System.out.println("최대 5까지 입력하시길 바랍니다.");
-                    }
-                    if(!(this.row>5)){
-                        break;
-                    } else{
-                        System.out.println("에러: 행과 열은 최대 5까지입니다.");
-                    }
-                }
-
-                while(true) {
-                    try {
-                        System.out.println("열을 입력하세요(max:5)");
-                        col = scan.nextInt();
-                    } catch (InputMismatchException | ArrayIndexOutOfBoundsException f) {
-                        f.printStackTrace();
-                        scan = new Scanner(System.in);
-                        System.out.println("최대 5까지 입력하시길 바랍니다.");
-                    }
-                    if (!(col>5)){
-                        break;
-                    } else {
-                        System.out.println("에러: 행과 열은 최대 5까지입니다.");
-                    }
-                }
-            break;
-        }
-        if(seat[row][col].equals("___")){
-            seat[row-1][col-1] = userId;
-            System.out.println((row)+"행"+(col)+"열 좌석이 예약되었습니다.");
-            cinemaPrint();
-            cinemaSeatQuery();
-        } else{
-            System.out.println("이미 예약된 좌석입니다");
-        }
-
+	public void ReserveTicket()  {
+        
     }
 
 	@Override
@@ -69,8 +29,18 @@ public class Reserveticket implements ReserveMovie{
 
 	@Override
 	public void CheckSeat() {
-		// TODO Auto-generated method stub
-		
+		char row = 'A';
+		for(int i=0; i<5; i++){
+			System.out.print(row +" Line: ");row++;
+			for(int j=0; j<5; j++){
+			String seat = (this.seat[i][j].equals("___"))? "◻": "◼";
+			System.out.print((j+1)+seat +"  ");
+			}
+			System.out.print(System.lineSeparator());
+			}
+		TimeUnit.SECONDS.sleep(5);
+		controller();
+		}		
 	}
 
 	@Override
