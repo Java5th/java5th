631497088c71e03064a340eeeaafdd0dc0110f63
Hyongyi <102146170+Hyongyi@users.noreply.github.com>
2022-06-10 17:04:33 +0900

    SelectMovie 추가

---
+++ Project/src/miniproject1/Selectmovie.java
@@ -0,0 +1,35 @@
+package miniproject1;
+
+public class Selectmovie implements ReserveMovie {
+
+	@Override
+	public void LogIn() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void ReserveTicket() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void CancleTicket() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void CheckSeat() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void Close() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
