e1019fbb3a6c7c9f1030bfb8d6568dbc8768ab06
bKfe-j <102991624+bKfe-j@users.noreply.github.com>
2022-06-10 17:52:38 +0900

    Update Reserveticket.java
    별도 클래스 생성해서 Main 매소드로 실행해본결과 출력 잘됩니다.
    실행해보려면 중간에 주석처리된 부분을 클래스 생성 후 복사하시면 됩니다.

--- Project/src/miniproject1/Reserveticket.java
+++ Project/src/miniproject1/Reserveticket.java
@@ -2,15 +2,46 @@ package miniproject1;
 
 public class Reserveticket implements ReserveMovie{
 	int row, col, selectNum;
-	Scanner scan = new Scanner(System.in);
-	String userId;
-	String userPw;
-	String[][] seat = new String[5][5];
-	public static void main(String[] args){
-		CheckSeat();
-	}
+    Scanner scan = new Scanner(System.in);
+    String userId;
+    String userPw;
+    String[][] seat = new String[5][5];
+    public void resetSeat() {
+        for(int i=0; i<5; i++){
+            for(int j=0; j<5; j++){
+                seat[i][j] = "___";
+            }
+        }
+    }
+    public void CheckSeat() throws InterruptedException, IOException {
+        char row = 'A';
+        for(int i=0; i<5; i++){
+            System.out.print(row +" Line: ");row++;
+            for(int j=0; j<5; j++){
+                String seat = (this.seat[i][j].equals("___"))? "◻": "◼";
+                System.out.print((j+1)+seat +"  ");
+            }
+            System.out.print(System.lineSeparator());
+        }
+        TimeUnit.SECONDS.sleep(5);
+// 		controller();
+    }
+}
+/*
+----------별도 클래스 생성 후 실행파일
+public static void main(String[] args) throws IOException, InterruptedException {
+        Reserveticket rt = new Reserveticket();
+        rt.resetSeat();
+        rt.CheckSeat();
 
+    }
+*/
 	@Override
+	public void Close() {
+		// TODO Auto-generated method stub
+		
+	}
+@Override
 	public void LogIn(String id,int password) {
 		// TODO Auto-generated method stub
 		
@@ -27,26 +58,5 @@ public class Reserveticket implements ReserveMovie{
 		
 	}
 
-	@Override
-	public void CheckSeat() {
-		char row = 'A';
-		for(int i=0; i<5; i++){
-			System.out.print(row +" Line: ");row++;
-			for(int j=0; j<5; j++){
-			String seat = (this.seat[i][j].equals("___"))? "◻": "◼";
-			System.out.print((j+1)+seat +"  ");
-			}
-			System.out.print(System.lineSeparator());
-			}
-		TimeUnit.SECONDS.sleep(5);
-		controller();
-		}		
-	}
-
-	@Override
-	public void Close() {
-		// TODO Auto-generated method stub
-		
-	}
 
 }
