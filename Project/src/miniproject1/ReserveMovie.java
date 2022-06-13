f7226517e932b21834474f2ad491ccd0773a3d66
saron312 <67586183+saron312@users.noreply.github.com>
2022-06-13 15:44:42 +0900

    Add files via upload

--- Project/src/miniproject1/ReserveMovie.java
+++ Project/src/miniproject1/ReserveMovie.java
@@ -1,12 +1,75 @@
-package miniproject1;
+import java.util.Scanner;
 
-public interface ReserveMovie {
-	
-	
-	public void LogIn(); 
-	public void ReserveTicket(); 
-	public void CancleTicket(); 
-	public void CheckSeat(); 
-	public void Close(); 
-	
+public class ReserveMovie {
+	public static void main(String[] args) {
+		Movie mv = new Movie();
+		boolean startMenu = true;
+		Scanner scan = new Scanner(System.in);
+		int select = 0;
+
+		while (startMenu) {
+			boolean movieChartMenu = true;
+
+			//예매하기 메뉴
+			System.out.println("------------------");
+			System.out.println("1.예매하기  | 2.종료");
+			System.out.println("------------------");
+			System.out.print("번호 입력>>");
+			select = scan.nextInt();
+
+			switch (select) {
+			case 1:
+				while (movieChartMenu) {
+					boolean reservTimeMenu = true;
+					mv.movieChart();
+					mv.selectMenu();
+					select = scan.nextInt();
+
+					if (select < 4) {
+						while (reservTimeMenu) {
+						mv.reservDate();
+						mv.selectMenu();
+						select = scan.nextInt();
+
+							if (select < 4) {
+								mv.reservTime(mv.time);
+								mv.selectMenu();
+								select = scan.nextInt();
+								if(select !=4) {
+									reservTimeMenu = false;
+									movieChartMenu = false;
+									startMenu =false;
+									System.out.println("------------------");
+									System.out.println("<<좌석선택>>");
+								}else if(select > 4){
+									mv.selectError();
+								}
+							} else if (select == 4) {
+								reservTimeMenu = false;							
+							} else if (select > 4) {
+								mv.selectError();
+								break;						
+							}
+						}
+						
+					} else if (select == 4) {
+						movieChartMenu = false;
+					} else if (select > 4) {
+						mv.selectError();
+					}
+
+				}
+				break;
+			case 2:
+				System.out.println("프로그램 종료");
+				startMenu = false;
+				break;
+			default:
+				mv.selectError();
+				break;
+			}
+		}
+		select = scan.nextInt();
+
+	}
 }
