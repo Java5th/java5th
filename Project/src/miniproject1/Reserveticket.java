bf0bf3d124c1dc64807df172f387ca8d8fff430d
bKfe-j <102991624+bKfe-j@users.noreply.github.com>
2022-06-13 16:50:50 +0900

    Update Reserveticket.java

--- Project/src/miniproject1/Reserveticket.java
+++ Project/src/miniproject1/Reserveticket.java
@@ -1,4 +1,4 @@
-package miniproject1;
+package com.miniproject1;
 
 public class Reserveticket implements ReserveMovie{
 
@@ -79,27 +79,27 @@ import java.util.concurrent.TimeUnit;
     }
 
 }
-	@Override
-	public void Close() {
-		// TODO Auto-generated method stub
-		
-	}
-@Override
-	public void LogIn(String id,int password) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void Close() {
+        // TODO Auto-generated method stub
 
-	@Override
-	public void ReserveTicket()  {
-        
+    }
+    @Override
+    public void LogIn(String id,int password) {
+        // TODO Auto-generated method stub
+
     }
 
-	@Override
-	public void CancleTicket() {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void ReserveTicket()  {
+
+    }
+
+    @Override
+    public void CancleTicket() {
+        // TODO Auto-generated method stub
+
+    }
 
 
 }
